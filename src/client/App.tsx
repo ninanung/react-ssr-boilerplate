053ce962372c5fc207af53e21011c1a816bc5cc4
Seungjae Jun <ninanung0503@gmail.com>
2020-01-16 19:35:45 +0900

    router, helmet 설정

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Helmet from "react-helmet";
+
+import Home from "./pages/Home";
+import News from "./pages/News";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
     render() {
